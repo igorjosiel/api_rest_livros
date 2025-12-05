3309143349fba51d56a12341f655aecb643d885a
Igor Josiel <igorjosielvitalino@gmail.com>
2025-12-04 22:11:34 -0300

    feature: Adiciona a rota para buscar um livro específico.

--- src/app.js
+++ src/app.js
@@ -3,22 +3,34 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const livros = [
+const books = [
   { id: 1, titulo: "O Senhor dos Anéis" },
   { id: 2, titulo: "1984" },
   { id: 3, titulo: "O Pequeno Príncipe" },
 ];
+
+function searchBook(id) {
+  return books.findIndex((book) => {
+    return book.id === Number(id);
+  });
+}
 
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
 });
 
 app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+  res.status(200).json(books);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const index = searchBook(req.params.id);
+
+  res.status(200).json(books[index]);
 });
 
 app.post("/livros", (req, res) => {
-  livros.push(req.body);
+  books.push(req.body);
   res.status(201).send("Livro adicionado com sucesso!");
 });
 
